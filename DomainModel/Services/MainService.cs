e239c088ce335e7f7d1d417358fe7ce854264c84
tori690 <tori690@yandex.ru>
2020-10-23 20:37:43 +0300

    Описан класс-сервис для работы с печатными формами.

--- DomainModel/Services/MainService.cs
+++ DomainModel/Services/MainService.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using Presentation;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DomainModel
 {
-    class MainService
+    public class MainService : IMainService
     {
+        private readonly IMainModel _model; // Модель данных
+
+        public string SavedPrintFormFullPath { get { return _model.SavedPrintFormFullPath; } }
+
+        public List<string> FieldList
+        {
+            get { return _model.FieldListAsList; }
+        }
+
+        public MainService()
+        {
+            _model = new MainModel();
+        }
+
+        public bool FieldListContainsName(string name) => _model.FieldListContainsName(name);                           // Проверить, содержится ли поле в списке
+        public bool IsFileExist(string fileName) => _model.IsFileExist(fileName);                                       // Проверить файл на существование
+        public string GetFieldValue(string selectedFieldName) => _model.GetFieldValue(selectedFieldName);               // Получить значение поля
+        public void OpenTemplate(string filePath) => _model.OpenTemplate(filePath);                                     // Открыть шаблон
+        public void SetFieldValue(string fieldName, string fieldValue) => _model.SetFieldValue(fieldName, fieldValue);  // Установить значение поля
+
+        // Сохранить печатную форму в файл
+        public void SavePrintForm(string printFormPath)
+        {
+            try
+            {
+                _model.SaveTemplateToFile(printFormPath);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        // Отправить форму на печать
+        public void ToPrintForm()
+        {
+            try
+            {
+                _model.ToPrintForm();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
