db22c6ce66775d517ad84051be9bcf4c559f42e9
tori690 <tori690@yandex.ru>
2020-11-04 12:05:15 +0300

    Сокращение лишнего кода. Приведение структуры кода к паттерну MVP.

--- DomainModel/Services/EmailService.cs
+++ DomainModel/Services/EmailService.cs
@@ -4,29 +4,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net.Mail;
+using System.Net;
 
 namespace DomainModel
 {
     public class EmailService : IEmailService
     {
-        private readonly IEmailModel _model; // Модель данных
+        private readonly IEmail _model; // Модель данных
         private readonly IEmailRepository _repository; // Взаимодействие с БД
+
+        private Dictionary<string, int> smtpDic = new Dictionary<string, int>()
+        {
+            {"smtp.yandex.ru",  25 },
+            {"smtp.mail.ru",    465 },
+            {"smtp.gmail.com",  465 }
+        };
 
         public Dictionary<string, int> SmtpDic
         {
-            get { return _model.SmtpDic; }
+            get { return smtpDic; }
         }
 
         public EmailService()
         {
-            _model = new EmailModel();
+            _model = new Email();
             _repository = new EmailRepository();
         }
 
         // Проверить файл на существование
         public bool IsFileExist(string fileName)
         {
-            return _model.IsFileExist(fileName);
+            return File.Exists(fileName);
         }
 
         // Отправить письмо
@@ -34,8 +44,33 @@ namespace DomainModel
         {
             try
             {
-                _model.SendToEmail();
-                _repository.AddEmailLog(_model.Model);
+                using (MailMessage message = new MailMessage(_model.SenderEmail, _model.RecipientEmail))
+                {
+                    message.Subject = _model.MailSubject;
+                    message.Body = _model.MailBody;
+                    message.IsBodyHtml = false;
+                    message.Attachments.Add(new Attachment(_model.Attachment));
+
+                    using (SmtpClient client = new SmtpClient(_model.SmtpServer, _model.SmtpPort))
+                    {
+                        client.EnableSsl = true;
+                        client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                        client.UseDefaultCredentials = false;
+
+                        client.Credentials = new NetworkCredential(_model.SenderEmail, _model.SenderPassword);
+                        try
+                        {
+                            client.Send(message);
+                            _model.DateTimeOfSendToEmail = DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss");
+                        }
+                        catch (Exception ex)
+                        {
+                            throw ex;
+                        }
+                    }
+                }
+
+                _repository.AddEmailLog(_model); // Логгируем в бд
             }
             catch (Exception ex)
             {
@@ -46,29 +81,29 @@ namespace DomainModel
         // Установить параметры письма
         public void SetMail(string subject, string body, string attach)
         {
-            _model.Model.MailSubject = subject;
-            _model.Model.MailBody = body;
-            _model.Model.Attachment = attach;
+            _model.MailSubject = subject;
+            _model.MailBody = body;
+            _model.Attachment = attach;
         }
 
         // Установить параметры получателя
         public void SetRecipient(string recipientEmail)
         {
-            _model.Model.RecipientEmail = recipientEmail;
+            _model.RecipientEmail = recipientEmail;
         }
 
         // Установить параметры отправителя
         public void SetSender(string senderEmail, string senderPassword)
         {
-            _model.Model.SenderEmail = senderEmail;
-            _model.Model.SenderPassword = senderPassword;
+            _model.SenderEmail = senderEmail;
+            _model.SenderPassword = senderPassword;
         }
 
         // Установить параметры Smtp-сервера
         public void SetSmtp(string smtpServer, int smtpPort)
         {
-            _model.Model.SmtpServer = smtpServer;
-            _model.Model.SmtpPort = smtpPort;
+            _model.SmtpServer = smtpServer;
+            _model.SmtpPort = smtpPort;
         }
     }
 }
--- DomainModel/Services/MainService.cs
+++ DomainModel/Services/MainService.cs
@@ -1,40 +1,167 @@
-﻿using Presentation;
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Presentation;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace DomainModel
 {
     public class MainService : IMainService
     {
-        private readonly IMainModel _model; // Модель данных
+        private readonly IPrintForm _model; // Модель данных
         private readonly IMainRepository _repository; // Взаимодействие с БД
 
-        public string SavedPrintFormFullPath { get { return _model.Model.SavedPrintFormFullPath; } }
+        public string SavedPrintFormFullPath { get { return _model.SavedPrintFormFullPath; } }
 
+        private Dictionary<string, string> dicTemplateFields; // Словарь, содержащий поле шаблона и указанное значение
         public List<string> FieldList
         {
-            get { return _model.FieldListAsList; }
+            get { return dicTemplateFields?.Keys.ToList(); }
         }
 
         public MainService()
         {
-            _model = new MainModel();
+            dicTemplateFields = new Dictionary<string, string>();
+            _model = new PrintForm();
             _repository = new MainRepository();
         }
 
-        public bool FieldListContainsName(string name) => _model.FieldListContainsName(name);                           // Проверить, содержится ли поле в списке
-        public bool IsFileExist(string fileName) => _model.IsFileExist(fileName);                                       // Проверить файл на существование
-        public string GetFieldValue(string selectedFieldName) => _model.GetFieldValue(selectedFieldName);               // Получить значение поля
-        public void OpenTemplate(string filePath) => _model.OpenTemplate(filePath);                                     // Открыть шаблон
-        public void SetFieldValue(string fieldName, string fieldValue) => _model.SetFieldValue(fieldName, fieldValue);  // Установить значение поля
+        // Проверить, содержится ли поле в списке
+        public bool FieldListContainsName(string name)
+        {
+            return dicTemplateFields.Keys.Contains(name);
+        }
+
+        // Проверить файл на существование
+        public bool IsFileExist(string fileName)
+        {
+            return File.Exists(fileName);
+        }
+
+        // Получить значение поля
+        public string GetFieldValue(string selectedFieldName)
+        {
+            if (dicTemplateFields.Keys.Contains(selectedFieldName))
+            {
+                return dicTemplateFields[selectedFieldName];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        // Открыть шаблон
+        public void OpenTemplate(string filePath)
+        {
+            try
+            {
+                _model.TemplateFilePath = filePath;
+                dicTemplateFields.Clear();
+                _model.SavedPrintFormFullPath = null;
+                byte[] textByteArray = File.ReadAllBytes(filePath);                                 // Получаем массив байтов из нашего файла
+
+                using (MemoryStream stream = new MemoryStream())                                    // Начинаем работу с потоком
+                {
+                    stream.Write(textByteArray, 0, textByteArray.Length);                           // Записываем в поток наш word-файл
+
+                    using (WordprocessingDocument doc = WordprocessingDocument.Open(stream, true))  // Открываем документ из потока с возможностью редактирования
+                    {
+                        var bookMarks = FindBookmarks(doc.MainDocumentPart.Document);               // Ищем все закладки в документе
+
+                        foreach (var end in bookMarks)                                              // Добавляем найденные закладки в словарь
+                        {
+                            dicTemplateFields.Add(end.Key, string.Empty);                           // end.Key содержит имена наших закладок
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }                                 
+
+        // Установить значение поля
+        public void SetFieldValue(string fieldName, string fieldValue)
+        {
+            if (dicTemplateFields.ContainsKey(fieldName))
+            {
+                dicTemplateFields[fieldName] = fieldValue;
+            }
+        }
 
         // Сохранить печатную форму в файл
         public void SavePrintForm(string printFormPath)
         {
             try
             {
-                _model.SaveTemplateToFile(printFormPath);
-                _repository.AddPrintFormLog(_model.Model);
+                try
+                {
+                    // Получаем массив байтов из нашего файла
+                    byte[] textByteArray = File.ReadAllBytes(_model.TemplateFilePath);
+
+                    // Начинаем работу с потоком
+                    using (MemoryStream stream = new MemoryStream())
+                    {
+                        // Записываем в поток наш word-файл
+                        stream.Write(textByteArray, 0, textByteArray.Length);
+                        // Открываем документ из потока с возможностью редактирования
+                        using (WordprocessingDocument doc = WordprocessingDocument.Open(stream, true))
+                        {
+                            // Изменяем шрифт по-умолчанию
+                            RunProperties rPr = new RunProperties(
+                                new RunFonts()
+                                {
+                                    HighAnsi = "Times New Roman"
+                                },
+                                new FontSize()
+                                {
+                                    Val = new StringValue("28")
+                                });
+
+                            // Ищем все закладки в документе
+                            var bookMarks = FindBookmarks(doc.MainDocumentPart.Document);
+
+                            foreach (var end in bookMarks)
+                            {
+                                foreach (var field in dicTemplateFields)
+                                {
+                                    if (end.Key == field.Key)
+                                    {
+                                        var textElement = new Text(field.Value ?? string.Empty);
+                                        // Далее данный текст добавляем в закладку
+                                        Run runElement = new Run();
+                                        runElement.AppendChild(textElement);
+                                        runElement.PrependChild(rPr.CloneNode(true));
+                                        end.Value.InsertAfterSelf(runElement);
+                                    }
+                                }
+                            }
+                        }
+                        // Записываем всё в наш файл
+                        string filePath = Directory.GetCurrentDirectory() + "\\" + printFormPath;
+                        File.WriteAllBytes(filePath, stream.ToArray());
+                        _model.SavedPrintFormFullPath = filePath;
+                        _model.DateTimeOfSaveForm = DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss");
+                        _model.DateTimeOfSendToPrint = String.Empty;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                _repository.AddPrintFormLog(_model); // Логгируем в БД
             }
             catch (Exception ex)
             {
@@ -47,21 +174,83 @@ namespace DomainModel
         {
             try
             {
-                if (String.IsNullOrEmpty(_model.Model.DateTimeOfSendToPrint))
+                if (String.IsNullOrEmpty(_model.DateTimeOfSendToPrint))
                 {
-                    _model.ToPrintForm();
-                    _repository.UpdatePrintFormLog(_model.Model);
+                    ToPrintPrintForm();
+                    _repository.UpdatePrintFormLog(_model);
                 }
                 else
                 {
-                    _model.ToPrintForm();
-                    _repository.AddPrintFormLog(_model.Model);
+                    ToPrintPrintForm();
+                    _repository.AddPrintFormLog(_model);
                 }
             }
             catch (Exception ex)
             {
                 throw ex;
             }
+        }
+
+        // Отправить форму на печать
+        private void ToPrintPrintForm()
+        {
+            try
+            {
+                using (PrintDialog pd = new PrintDialog())
+                {
+                    pd.ShowDialog();
+                    ProcessStartInfo info = new ProcessStartInfo($"{_model.SavedPrintFormFullPath}");
+                    info.WindowStyle = ProcessWindowStyle.Hidden;
+                    info.Verb = "PrintTo";
+                    info.Arguments = pd.PrinterSettings.PrinterName;
+                    info.CreateNoWindow = true;
+                    Process.Start(info);
+                    _model.DateTimeOfSendToPrint = DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        // Найти все закладки в документе
+        private static Dictionary<string, BookmarkEnd> FindBookmarks(OpenXmlElement documentPart, Dictionary<string, BookmarkEnd> outs = null, Dictionary<string, string> bStartWithNoEnds = null)
+        {
+            if (outs == null) { outs = new Dictionary<string, BookmarkEnd>(); } // outs - конечный результат
+            if (bStartWithNoEnds == null) { bStartWithNoEnds = new Dictionary<string, string>(); } // bStartWithNoEnds - словарь, который будет содержать только начало закладок, чтобы потом по ним находить соответствующие им концы закладок
+
+            // Проходимся по всем элементам на странице Word-документа
+            foreach (var docElement in documentPart.Elements()) // documentPart - элемент Word-документа
+            {
+                // BookmarkStart определяет начало закладки в рамках документа
+                // маркер начала связан с маркером конца закладки
+                if (docElement is BookmarkStart)
+                {
+                    var bookmarkStart = docElement as BookmarkStart;
+                    if (bookmarkStart.Name != "_GoBack") // Игнорировать стандартный метод возврата
+                    {
+                        bStartWithNoEnds.Add(bookmarkStart.Id, bookmarkStart.Name); // Записываем id и имя закладки
+                    }
+                }
+
+                // BookmarkEnd определяет конец закладки в рамках документа
+                if (docElement is BookmarkEnd)
+                {
+                    var bookmarkEnd = docElement as BookmarkEnd;
+                    foreach (var startName in bStartWithNoEnds)
+                    {
+                        // startName.Key как раз и содержит id закладки
+                        // здесь проверяем, что есть связь между началом и концом закладки
+                        if (bookmarkEnd.Id == startName.Key)
+                            outs.Add(startName.Value, bookmarkEnd); // В конечный список добавляем то, что нам и нужно получить
+                    }
+                }
+                // Самовызывае, чтобы пройтись по всем элементам word-документа
+                FindBookmarks(docElement, outs, bStartWithNoEnds);
+            }
+
+            return outs;
         }
     }
 }
